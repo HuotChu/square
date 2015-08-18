79732733b0f0aa028972099d93bd6ee682cd6645
blujagu <lawebtek@gmail.com>
2015-08-17 23:06:57 -0600

    Improved view construction

--- examples/js/mods/product/productModel.js
+++ examples/js/mods/product/productModel.js
@@ -1,7 +1,7 @@
 define(['js/model', 'js/notify', 'js/request'], function (model, notify, request) {
     'use strict';
     return new Promise(function (resolve, reject) {
-        request('json/devices.json').then(function(deviceData) {
+        request('json/devices.json').then(function (deviceData) {
             // use the data Promise to pass the XHR object into the deviceData argument
             // which ensures the request has completed before executing this code
 
