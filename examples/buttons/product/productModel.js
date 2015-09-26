9bbdba12aff9fddc39c0defae92ec17d11f7b056
blujagu <lawebtek@gmail.com>
2015-09-26 14:13:52 -0600

    Updated to work with new model API

--- examples/buttons/product/productModel.js
+++ examples/buttons/product/productModel.js
@@ -6,11 +6,27 @@ define(['model', 'notify', 'request'], function (model, notify, request) {
             // which ensures the request has completed before executing this code
 
             // parse the response json data and grab the devices array to assign to allData
-            var allData = JSON.parse(deviceData['response'])['devices'],
-                productModel = model.create('product').insert('products', allData);
+            var allData = JSON.parse(deviceData['response'])['devices'];
 
+            // create a model named productModel
+            var productModel = model.create('productModel');
+
+            // create a named data array and insert into the model
+            var dataArray = productModel.newDataArray('devices', '');
+
+            // copy the data array we got from the server in the dataArray we just made
+            // this will convert all the data into dataObjects and add query to the array
+            dataArray.cloneArray(allData);
+
+            // TEST
+            //var productObj = dataArray.query('isEqual', ['id','s4']);
+            //console.log("Should be 'Sony 4': ", productObj.get('product'));
+
+            // register the publisher event handler on the model so temple.js can attach it to templates
             productModel.register('publisher', 'click', notify.publisher);
-            resolve(model.getModel('product'));
+
+            // resolve the promise and pass the model to the Promise handler
+            resolve(productModel);
         })
     });
 });
