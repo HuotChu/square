c33c7f7bddba179bc90fa57aec9a58482b671399
sbishop <sbishop@4qbase.com>
2015-09-27 18:08:10 -0600

    Moved controller code to controller

--- examples/Demo_Todos/todoView.js
+++ examples/Demo_Todos/todoView.js
@@ -1,36 +1,7 @@
-define(['temple', 'notify', 'todoModel'], function (temple, notify, todoModel) {
+define(['temple', 'todoController'], function (temple, todoController) {
     return new Promise(function (resolve, reject) {
         temple.getTemplate('todos.html').then(function (view) {
-            todoModel.then(function (model) {
-                model.register('publisher', 'click', notify.publisher);
-
-                model.register('addTodo', 'submit', function (e) {
-                    var newTodo = document.querySelector('#new-todo'),
-                        val = newTodo.value,
-                        todos = model.getNodeFromPath('todos'),
-                        last = todos[todos.length - 1].get('id'),
-                        todo = temple.getTemplate('todo.html', true),
-                        dataObj = model.add('todos', {title: val, done: false, id: ++last});
-
-                    e.preventDefault();
-                    e.stopPropagation();
-
-                    todo.then(function (todoDom) {
-                        var baseNode = document.querySelector('#todo-list');
-
-                        baseNode.appendChild(temple.toDom(todoDom, dataObj));
-                        newTodo.value = '';
-                    });
-                });
-
-                model.register('removeTodo', 'click', function (e) {
-                    var target = e.target.offsetParent,
-                        id = target.id.replace(/^todo\-/, '');
-
-                    model.remove('todos', 'isEqual', ['id', id]);
-                    target.parentElement.removeChild(target);
-                });
-
+            todoController.then(function (model) {
                 view = temple.toDom(view, model);
 
                 resolve(view);
