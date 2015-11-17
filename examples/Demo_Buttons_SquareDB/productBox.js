44011c72de6932cd8e7a8587ad40eb4b871ab243
blujagu <lawebtek@gmail.com>
2015-11-16 23:57:12 -0700

    Demo 1 updates

--- examples/Demo_Buttons_SquareDB/productBox.js
+++ examples/Demo_Buttons_SquareDB/productBox.js
@@ -6,49 +6,45 @@
 define(['box', 'productModel'], function(Box, productModel) {
     'use strict';
 
-    return new Promise(function (resolve) {
-        productModel.then(function (model) {
-            var boxConfig = {
-                model: model,
+    productModel.then(function (model) {
+        var boxConfig = {
+            model: model,
 
-                data: {
-                    devices: model.select('*').from('Devices').go()
-                },
+            data: {
+                devices: model.select('product, id').from('Devices').go()
+            },
 
-                target: document.querySelector('#button-demo'),
+            target: document.querySelector('#button-demo'),
 
-                template: 'product.html',
+            template: 'product.html',
 
-                domEvents: [
-                    {
-                        event: 'click',
-                        id: 'showProduct_',
-                        callback: function (id, domNode, box) {
-                            box.index['desc'].className = 'hidden';
-                            model.select('desc').from('Devices').where('id', '===', id).go();
-                        }
+            domEvents: [
+                {
+                    event: 'click',
+                    id: 'showProduct_',
+                    callback: function (id, domNode, box) {
+                        box.index['desc'].className = 'hidden';
+                        model.select('desc')
+                                  .from('Devices')
+                                  .where('id', '===', id).go();
                     }
-                ],
+                }
+            ],
 
-                modelEvents: [
-                    {
-                        event: 'Products.Devices.desc.read',
-                        id: 'desc',
-                        callback: function (event, descriptionNode) {
-                            setTimeout(function () {
-                                descriptionNode.innerHTML = event.detail.value[0]['desc'];
-                                descriptionNode.className = 'show';
-                            }, 500);
-                        }
+            modelEvents: [
+                {
+                    event: 'Products.Devices.desc.read',
+                    id: 'desc',
+                    callback: function (event, descriptionNode) {
+                        setTimeout(function () {
+                            descriptionNode.innerHTML = event.detail.value[0]['desc'];
+                            descriptionNode.className = 'show';
+                        }, 500);
                     }
-                ]
-            };
+                }
+            ]
+        };
 
-            var box = new Box(boxConfig);
-
-            box.build().then(function (html) {
-                resolve(html);
-            });
-        });
+        new Box(boxConfig).build();
     });
 });
