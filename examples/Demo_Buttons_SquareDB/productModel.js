0533d5b8a713a3b99035e306d14615774fc8000e
blujagu <lawebtek@gmail.com>
2015-11-24 23:28:12 -0700

    tutorial updates

--- examples/Demo_Buttons_SquareDB/productModel.js
+++ examples/Demo_Buttons_SquareDB/productModel.js
@@ -12,7 +12,7 @@ define(['db', 'eventHub', 'request', 'lobro'], function (db, eventHub, request, 
         if (model.isCurrent('Products', 1)) {
             // load localStorage data into the model
             model.sync('Products');
-            // when using eventHub with lobro, we can enable events so lobro will auto-update localStorage when data changes
+            // enable events so lobro will auto-update localStorage when data changes
             model.enableEvents();
             // resolve and pass the model to the handler
             resolve(model);
@@ -27,8 +27,11 @@ define(['db', 'eventHub', 'request', 'lobro'], function (db, eventHub, request, 
                 // converts a collection of objects to relational structure
                 model.insertJsonInto('Devices')(allData);
                 if (model.enableEvents) {
+                    // ensure no old localStorage model exists
                     model.clearLocal();
+                    // copy model to localStorage with version 1
                     model.persist(model, 1);
+                    // tell lobro to update localStorage when the model data changes
                     model.enableEvents();
                 }
                 // resolve and pass the model to the handler
